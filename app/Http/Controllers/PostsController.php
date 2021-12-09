e601774d488a337b41cf20d15b2dc7396c0306c5
Andrew <feshykandrew.work@gmail.com>
2021-12-09 12:36:24 +0200

    add validations

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -26,9 +26,9 @@ class PostsController extends Controller
     public function store()
     {
         $data = request()->validate([
-            'title' => 'string',
-            'content' => 'string',
-            'image' => 'string',
+            'title' => 'required|string',
+            'content' => 'required|string',
+            'image' => 'required|string',
             'category_id' => '',
             'tags' => '',
         ]);
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,23 +4,39 @@
         @csrf
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" id="title" name="title" placeholder="Title">
+            <input
+                value="{{ old('title') }}"
+                type="text" class="form-control" id="title" name="title" placeholder="Title">
+            @error('title')
+            <p class="alert-danger">{{ $message }}</p>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="content" class="form-label">Content</label>
-            <textarea class="form-control" id="content" name="content" placeholder="Content" rows="3"></textarea>
+            <textarea class="form-control" id="content" name="content" placeholder="Content" rows="3">{{ old('content') }}</textarea>
+            @error('content')
+            <p class="alert-danger">{{ $message }}</p>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="image" class="form-label">Image</label>
-            <input class="form-control" id="image" placeholder="Image" name="image">
+            <input value="{{ old('image') }}" class="form-control" id="image" placeholder="Image" name="image">
+            @error('image')
+            <p class="alert-danger">{{ $message }}</p>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="category" class="form-label">Category</label>
             <select class="form-select" name="category_id" id="category">
                 @foreach($categories as $category)
-                    <option value="{{ $category->id }}">{{ $category->title }}</option>
+                    <option
+                        {{ old('category_id') == $category->id ? 'selected' : '' }}
+                        value="{{ $category->id }}">{{ $category->title }}</option>
                 @endforeach
             </select>
+            @error('category')
+            <p class="alert-danger">{{ $message }}</p>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="tags" class="form-label">Tags</label>
@@ -29,6 +45,9 @@
                     <option value="{{ $tag->id }}">{{ $tag->title }}</option>
                 @endforeach
             </select>
+            @error('tags[]')
+            <p class="alert-danger">{{ $message }}</p>
+            @enderror
         </div>
         <button type="submit" class="btn btn-primary">Create</button>
     </form>
