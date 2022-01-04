d1e442c6539a747f72266e45c7ebb8555d3a9d77
Andrew <feshykandrew.work@gmail.com>
2022-01-04 17:02:38 +0200

    add pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,4 +5,7 @@
         <h2>{{ $post->id }}. <a href=" {{ route('post.show', $post->id) }}">{{ $post->title }}</a></h2>
     @endforeach
     <a href="{{ route('post.create') }}">Add new</a>
+    <div>
+        {{ $posts->links() }}
+    </div>
 @endsection
