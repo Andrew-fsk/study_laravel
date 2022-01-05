478bb642188de65f9ed5908336c5ab0be9604b8d
Andrew <feshykandrew.work@gmail.com>
2022-01-05 22:10:26 +0200

    change public page of post

--- app/Http/Controllers/Post/ShowController.php
+++ app/Http/Controllers/Post/ShowController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Post;
 
 class ShowController extends Controller
 {
     public function __invoke(Post $post)
     {
-        return view('posts.show', compact('post'));
+        $category_title = Category::find($post->category_id)->title;
+        return view('posts.show', compact('post', 'category_title'));
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,14 @@
 @section('content')
     <h2>{{ $post->id }}. {{ $post->title }}</h2>
     <p>{{ $post->content }}</p>
+    <img class="mb-3" src="{{$post->image}}" alt="{{ $post->title }}">
+    <p>Tags</p>
+    <p>
+    @foreach($post->tags as $tag)
+        {{ $tag->title }}@if( $loop->iteration != count($post->tags) ), @endif
+    @endforeach
+    </p>
+    <p>Category: {{ $category_title }}</p>
     <div class="d-flex">
         <div class="col">
             <a href="{{ route('post.index') }}" role="button"  class="btn btn-dark">Back</a>
